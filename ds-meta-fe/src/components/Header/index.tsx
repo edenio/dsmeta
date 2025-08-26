fae2b2e7a5ce2cf495b21c2b64c82ccdd4fea2c9
edenio <edenio_cdi@yahoo.com.br>
2025-08-26 18:56:39 -0300

    news functions

--- ds-meta-fe/src/components/Header/index.tsx
+++ ds-meta-fe/src/components/Header/index.tsx
@@ -12,7 +12,7 @@ function Header() {
             <h1>DSMeta</h1>
             <p>
               Desenvolvido por
-              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+              <a href="https://github.com/edenio">edenio.alv@gmail.com</a>
             </p>
         </div>
     </header>
